55745e7da41d8449217afc7dae7a93cedfca36ab
0x0ffff <990627820@qq.com>
2019-08-21 15:17:03 +0800

    _.range

--- main.js
+++ main.js
@@ -416,6 +416,26 @@
         return res;
     };
 
+    // 返回某一个范围内的数组成的数组
+    _.range = function(start, stop, step) {
+        if (stop == null) {
+            stop = start || 0;
+            start = 0;
+        }
+
+        step = step || 1;
+
+        // 长度
+        var length = Math.max(Math.ceil((stop - start) / step), 0);
+        var range = Array(length);
+
+        for (var i = 0; i < length; i++, start += step) {
+            range[i] = start;
+        }
+
+        return range;
+    };
+
 
 
     // -------Objects--------//
